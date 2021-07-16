6f27615ff46a5c2cccba000abbdb1ae48944e29b
carlos benavides <1992cab@gmail.com>
2021-07-16 16:28:21 -0500

    chore: working on main view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
-
+import { ReservationsService } from './services/reservations.service';
+import { RoomTypesService } from './services/room-types.service';
+import { SedesService } from './services/sedes.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
   title = 'hotel';
+  sedes: any;
+
+  constructor(private sedesService: SedesService, private roomTypesService: RoomTypesService, private reservationsService: ReservationsService) { }
+
+  ngOnInit() {
+    this.sedesService.getAll().subscribe(({ data }) => this.sedes = data);
+  }
 }
